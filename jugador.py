25752544c13b999ba18c86d847e32d30aea0c892
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-10-31 13:22:09 -0300

    proyecto juego python

---
+++ jugador.py
@@ -0,0 +1,23 @@
+class juego():
+    jugador
+    objetosColeccionables
+    objetosMobibles
+    objetosSolidos
+
+class jugador:
+    posicion
+    def moverse_adelante():
+        #actualizar la posicion basada en la direccion en la que esta mirando
+        
+    def girar_izquierda():
+        #actualizar el sprite del jugador dependiento en que direccion esta
+
+    def girar_derecha():
+        #actualizar el sprite del jugador dependiento en que direccion esta
+
+    def gritar(palabra):
+        print(palabra + "!")
+
+    def empujar():
+        # comprobar si hay una roca enfrente
+        # roca.mover()
