db5aa12ebf1b15118546aa09ea489556cf3bf90e
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-11-23 21:03:01 -0300

    aa

--- orden.py
+++ orden.py
@@ -8,3 +8,22 @@ def ordenar(lista):
                 lista[j] = lista[j + 1]
                 lista[j + 1] = temp
     return
+
+#hash sort
+def hash_sort(lista):
+    hash = {}
+    for i in lista:
+        if i in hash:
+            hash[i] += 1
+        else:
+            hash[i] = 1
+    lista = []
+    for i in sorted(hash.keys()):
+        lista += [i] * hash[i]
+
+#quick sort
+def quicksort(lista):
+    if len(lista) <= 1:
+        return lista
+    else:
+        return quicksort([x for x in lista[1:] if x < lista[0]]) + [lista[0]] + quicksort([x for x in lista[1:] if x >= lista[0]])
