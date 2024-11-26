629e75c25572c4ab8164914e3d2a51dda655f88f
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-11-26 18:42:20 -0300

    solucion errores print

--- hello.py
+++ hello.py
@@ -1,37 +1,46 @@
+#ejemplos
+
+#ejemplo de comentario
+
+#definicion de una funcion
 def funcion_ejemplo():
     print("funcion de ejemplo")
 
+#llamada a la funcion
 print("Hola, mundo!")
 
+#llamada a la funcion
 nombre = "Esteban"
 print("Hola %s como estas" % (nombre))
 
-print nombre.index("e")
+#uso de una funcion de variable
+print(nombre.index("e"))
 
 
+#uso de if
 name = "John"
 age = 23
 if name == "John" and age == 23:
-    print "tu nombre es John, y tu tienes 23 años."
+    print ("tu nombre es John, y tu tienes 23 años.")
 
 if name == "John" or name == "Rick":
-    print "Tu nombre es John o puede ser Rick."
+    print("Tu nombre es John o puede ser Rick.")
 
 
 primos = [2,3,5,7]
 for prime in primos:
-    print primos
+    print(primos) 
 
 
 count = 0
 while count < 5:
-    print count
+    print(count) 
     count += 1  # Esto es lo mismo que escribir:  count = count + 1
 
 class MyClass:
      variable = "blah"
      def function(self):
-          print "Este mensaje está dentro de una clase."
+          print() 
 
 myobjectx = MyClass()
 
@@ -47,6 +56,6 @@ librotelefonico = {
 print librotelefonico["juan"]
 
 for name, number in librotelefonico.iteritems():
-    print "Número telefónico de %s esta en %d" % (name, number)
+    print("Número telefónico de %s esta en %d" % (name, number))
 
 del librotelefonico["Juan"]
