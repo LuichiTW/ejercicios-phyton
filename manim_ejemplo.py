6bc1a151851ca8922b91e94b9e503d750cd203af
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-12-08 21:56:55 -0300

    manim

---
+++ manim_ejemplo.py
@@ -0,0 +1,14 @@
+
+
+#ejemplo usando manin para hacer una animacion de un cuadrado que se mueve
+from manim import *
+
+class CuadradoMovil(Scene):
+    def construct(self):
+        cuadrado = Square()
+        self.play(Create(cuadrado))
+        self.play(cuadrado.animate.shift(UP))
+        self.play(cuadrado.animate.shift(LEFT))
+        self.play(cuadrado.animate.shift(DOWN))
+        self.play(cuadrado.animate.shift(RIGHT))
+        self.play(FadeOut(cuadrado))
